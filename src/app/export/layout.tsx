a30b61151aeeeeab22ef0428b5bd1ef245e275e3
savinda savinda <savindasavinda01@gmail.com>
2025-08-01 15:57:24 +0000

    createConsoleError@https://9000-firebase-studio-1754060798295.cluster-ub

--- src/app/export/layout.tsx
+++ src/app/export/layout.tsx
@@ -19,7 +19,7 @@ export default function ExportLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn("font-body antialiased", "bg-white text-black")}>
+      <body className={cn("font-body antialiased", "bg-white text-black")} suppressHydrationWarning>
         {children}
       </body>
     </html>
