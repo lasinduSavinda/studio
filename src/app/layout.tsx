903debe7861dca88093df93c9139a6b6075ab2e7
lasinduSavinda <s92069862@ousl.lk>
2026-03-07 10:43:41 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9204801617185567"
      crossorigin="anonymous"></script>
+        <meta name="msvalidate.01" content="F9A98516248814EDA69BD8C7967EB05F" />
       </head>
       <body className={cn("min-h-screen bg-background font-body antialiased flex flex-col")} suppressHydrationWarning>
         <div className="flex-grow">
